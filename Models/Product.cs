611d7d8fae14d43a30fe35c60cd2e3aa083af23c
EdgarEldy <ed.eldy21@live.com>
2022-11-01 20:53:29 +0200

    Add Product model

---
+++ Models/Product.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace winforms_tutorial.Models
+{
+    [Table("Products")]
+    public class Product
+    {
+        // Constructor
+        public Product()
+        {
+            
+        }
+        
+        // Product properties goes here...
+    }
+}
