27762975c0256f41093ca28f4f02fd80ee2f928a
EdgarEldy <ed.eldy21@live.com>
2022-11-01 20:54:17 +0200

    Add Customer model

---
+++ Models/Customer.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace winforms_tutorial.Models
+{
+    [Table("Customers")]
+    public class Customer
+    {
+        // Constructor
+        public Customer()
+        {
+            
+        }
+        
+        // Customer properties goes here...
+    }
+}
