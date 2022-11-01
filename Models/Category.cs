8112783a9ab253f61cda5c913ee476d600b620a0
EdgarEldy <ed.eldy21@live.com>
2022-11-01 20:52:32 +0200

    Add Category model

---
+++ Models/Category.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace winforms_tutorial.Models
+{
+    [Table("Categories")]
+    public class Category
+    {
+        // Constructor
+    }
+    
+    // Category properties goes here...
+}
