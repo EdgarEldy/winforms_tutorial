572cc7193ada8209542c217c7e7785cc533811f8
EdgarEldy <ed.eldy21@live.com>
2022-11-01 20:55:37 +0200

    Add Order model

---
+++ Models/Order.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace winforms_tutorial.Models
+{
+    [Table("Orders")]
+    public class Order
+    {
+        // Constructor
+        public Order()
+        {
+            
+        }
+        
+        // Order properties goes here...
+    }
+}
