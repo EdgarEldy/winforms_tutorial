8622406791d312f2f682c4cbe5f9b202fd3395f1
EdgarEldy <ed.eldy21@live.com>
2022-11-02 15:52:31 +0200

    Add relationship to Order model

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -10,25 +11,28 @@ namespace winforms_tutorial.Models
         // Constructor
         public Customer()
         {
-            
+            this.Orders = new HashSet<Order>();
         }
-        
+
         // Customer properties goes here...
         [Key]
         public int Id { get; set; }
 
-        public string FirstName  { get; set; }
+        public string FirstName { get; set; }
 
-        public string LastName  { get; set; }
+        public string LastName { get; set; }
 
-        public string Tel  { get; set; }
+        public string Tel { get; set; }
 
-        public string Email  { get; set; }
+        public string Email { get; set; }
 
-        public string Address  { get; set; }
+        public string Address { get; set; }
 
         public DateTime CreatedAt { get; set; }
 
         public DateTime UpdatedAt { get; set; }
+
+        // Add relationship to Order model
+        public virtual ICollection<Order> Orders { get; set; }
     }
 }
