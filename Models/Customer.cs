90b566ac4041de0528860dda38269460d901c80b
EdgarEldy <ed.eldy21@live.com>
2022-11-02 15:22:59 +0200

    Add Customer model properties

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -1,3 +1,5 @@
+using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace winforms_tutorial.Models
@@ -12,5 +14,21 @@ namespace winforms_tutorial.Models
         }
         
         // Customer properties goes here...
+        [Key]
+        public int Id { get; set; }
+
+        public string FirstName  { get; set; }
+
+        public string LastName  { get; set; }
+
+        public string Tel  { get; set; }
+
+        public string Email  { get; set; }
+
+        public string Address  { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
     }
 }
