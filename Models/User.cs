2eeafbac4d47d66671742d0c2a77193a5b13dc1e
EdgarEldy <ed.eldy21@live.com>
2022-11-03 20:54:36 +0200

    Add User properties

--- Models/User.cs
+++ Models/User.cs
@@ -1,3 +1,5 @@
+using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace winforms_tutorial.Models
@@ -11,5 +13,22 @@ namespace winforms_tutorial.Models
         }
 
         // User properties goes here...
+        [Key] public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Tel { get; set; }
+
+        public string Email { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
     }
 }
