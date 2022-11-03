bfa170c4c0b344b3404db3aa26035afda8951c26
EdgarEldy <ed.eldy21@live.com>
2022-11-03 20:51:19 +0200

    Add User model

---
+++ Models/User.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace winforms_tutorial.Models
+{
+    [Table("Users")]
+    public class User
+    {
+        // Constructor
+        public User()
+        {
+        }
+
+        // User properties goes here...
+    }
+}
