a191899078ffb3ceabbe0be774988de98a684546
EdgarEldy <ed.eldy21@live.com>
2022-11-06 09:44:30 +0200

    Add UserRole model

---
+++ Models/UserRole.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace winforms_tutorial.Models
+{
+    [Table("UserRoles")]
+    public class UserRole
+    {
+        //Constructor   
+        public UserRole()
+        {
+        }
+
+        // Properties goes here...
+    }
+}
