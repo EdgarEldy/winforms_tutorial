413ea96bca7c5588a09ab87588568e41fab90723
EdgarEldy <ed.eldy21@live.com>
2022-11-06 09:42:47 +0200

    Add Role properties

--- Models/Role.cs
+++ Models/Role.cs
@@ -1,3 +1,5 @@
+using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace winforms_tutorial.Models
@@ -11,5 +13,12 @@ namespace winforms_tutorial.Models
         }
 
         // Role properties goes here...
+        [Key] public int Id { get; set; }
+
+        public string RoleName { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
     }
 }
