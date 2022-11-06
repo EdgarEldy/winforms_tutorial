e8a4cda51b1fd4aed1e87a5cf58a651f16baf2ca
EdgarEldy <ed.eldy21@live.com>
2022-11-06 09:42:03 +0200

    Add Role model

---
+++ Models/Role.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace winforms_tutorial.Models
+{
+    [Table("Roles")]
+    public class Role
+    {
+        // Constructor
+        public Role()
+        {
+        }
+
+        // Role properties goes here...
+    }
+}
